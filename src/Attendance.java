fbf0fdb12b1712f61e7575e365feb7bf06b2906a
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-11-21 14:18:32 -0500

    edited Attendance.java

--- src/Attendance.java
+++ src/Attendance.java
@@ -1,13 +1,26 @@
 package src;
+import java.io.File;
 import java.util.Calendar;
+import java.util.*;
 
 
 
 public class Attendance {
+
+    private ArrayList<Student> studlist;
     
     public static void main(String[] args) {
         
     }
 
+    private ArrayList<Student> loadstudents(String studfile){
+        
+    }
+
+    private void markAttendance(String studfile){
+        File studFile = new File(studfile);
+
+        
+    }
 
 }
