c30d68fe80860d271cfe05756a09530ea93e38b6
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-11-26 16:03:23 -0500

    Added Jdatepicker fucntionality to MarkAttendanceScreen.java

--- src/MarkAttendanceScreen.java
+++ src/MarkAttendanceScreen.java
@@ -1,17 +1,28 @@
 package src;
 
 import javax.swing.*;
+import javax.swing.JFormattedTextField.*;
+import javax.swing.JFormattedTextField.AbstractFormatter;
 import org.jdatepicker.*;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilCalendarModel;
+import org.jdatepicker.impl.UtilDateModel;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
 
 public class MarkAttendanceScreen extends JFrame{
 
     private MarkAttendanceScreen thisScr;
     private Attendance attendanceSheet;
     private JPanel buttonPanel, mainPanel;
-    private JComboBox studentBox, periodBox;
+    private JComboBox studentBox, periodBox, presenceBox;
     private JButton saveButton;
     private JButton closeButton;
 
@@ -22,7 +33,7 @@ public class MarkAttendanceScreen extends JFrame{
 
         buttonPanel = new JPanel();
         mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(3,1,5,15));
+        mainPanel.setLayout(new GridLayout(6,1,5,15));
 
         String [] studnames = attendanceSheet.getStudentNames().toArray(new String[attendanceSheet.getStudentNames().size()]);
 
@@ -35,21 +46,59 @@ public class MarkAttendanceScreen extends JFrame{
         periodBox = new JComboBox<String>(periods);
         mainPanel.add(periodBox);
 
+        mainPanel.add(new JLabel("Present/Absent"));
+        String [] presence = {"Present", "Absent"};
+        presenceBox = new JComboBox<String>(presence);
+        mainPanel.add(presenceBox);
+
         mainPanel.add(new JLabel("Date:"));
 
+        // Date 
+        
+        UtilCalendarModel model = new UtilCalendarModel();
+        
+        Properties p = new Properties();
+        p.put("text.today", "Today");
+        p.put("text.month", "Month");
+        p.put("text.year", "Year");
+        JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+        JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+
+        model.setSelected(true);
+        datePicker.setVisible(true);
+        mainPanel.add(datePicker);
+
+        // Date
         
 
         saveButton = new JButton("Save");
-        // saveButton.addActionListener(new SaveListener());
+        // saveButton.addActionListener(new saveListener());
         buttonPanel.add(saveButton);
 
-        closeButton = new JButton("Close");
-        // closeButton.addActionListener(new CloseListener());
+        closeButton = new JButton("Cancel");
+        // closeButton.addActionListener(new cancelListener());
         buttonPanel.add(closeButton);
 
         add(mainPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
         pack();
         setVisible(true);
+
     }
+
+
+
+    // save listener
+
+     // Calendar selectedValue = (Calendar) datePicker.getModel().getValue();
+        // Date selectedDate = selectedValue.getTime();
+        // System.out.println(selectedDate);
+
+
+    private class saveListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+        }
+      }
+
+    
 }
