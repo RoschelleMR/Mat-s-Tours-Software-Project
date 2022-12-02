f1ab959e039325b028c70143e39c737b83f81ad9
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-12-01 19:22:48 -0500

    Restored loginInfo.txt

--- src/Attendance.java
+++ src/Attendance.java
@@ -141,7 +141,7 @@ public class Attendance extends JFrame{
             while (studscan.hasNext()) {
                 String[] nextLine = studscan.nextLine().split(" ");
                 String firstname = nextLine[0];
-                String lastname = nextLine[1].replace(","," ");
+                String lastname = nextLine[1];
                 StringBuffer sb= new StringBuffer(lastname);    
                 sb.deleteCharAt(sb.length()-1);  
                 String name = nextLine[0] + " " + sb;
